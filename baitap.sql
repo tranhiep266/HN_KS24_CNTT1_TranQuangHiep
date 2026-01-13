39626c2e1373155a00785d1590e0c06d80dd6447
tranhiep266 <hiep5apy@gmail.com>
2026-01-13 07:46:37 +0700

    Add files via upload

--- baitap.sql
+++ baitap.sql
@@ -1,118 +1,88 @@
-CREATE DATABASE if not exists mini_project_ss08;
-USE mini_project_ss08;
-
--- Xóa bảng nếu đã tồn tại (để chạy lại nhiều lần)
-DROP TABLE IF EXISTS bookings;
-DROP TABLE IF EXISTS rooms;
-DROP TABLE IF EXISTS guests;
-
--- Bảng khách hàng
-CREATE TABLE guests
-(
-    guest_id   INT PRIMARY KEY AUTO_INCREMENT,
-    guest_name VARCHAR(100),
-    phone      VARCHAR(20)
-);
-
--- Bảng phòng
-CREATE TABLE rooms
-(
-    room_id       INT PRIMARY KEY AUTO_INCREMENT,
-    room_type     VARCHAR(50),
-    price_per_day DECIMAL(10, 0)
-);
-
--- Bảng đặt phòng
-CREATE TABLE bookings
-(
-    booking_id INT PRIMARY KEY AUTO_INCREMENT,
-    guest_id   INT,
-    room_id    INT,
-    check_in   DATE,
-    check_out  DATE,
-    FOREIGN KEY (guest_id) REFERENCES guests (guest_id),
-    FOREIGN KEY (room_id) REFERENCES rooms (room_id)
-);
-
-INSERT INTO guests (guest_name, phone)
-VALUES ('Nguyễn Văn An', '0901111111'),
-       ('Trần Thị Bình', '0902222222'),
-       ('Lê Văn Cường', '0903333333'),
-       ('Phạm Thị Dung', '0904444444'),
-       ('Hoàng Văn Em', '0905555555');
-
-INSERT INTO rooms (room_type, price_per_day)
-VALUES ('Standard', 500000),
-       ('Standard', 500000),
-       ('Deluxe', 800000),
-       ('Deluxe', 800000),
-       ('VIP', 1500000),
-       ('VIP', 2000000);
-
-INSERT INTO bookings (guest_id, room_id, check_in, check_out)
-VALUES (1, 1, '2024-01-10', '2024-01-12'), -- 2 ngày
-       (1, 3, '2024-03-05', '2024-03-10'), -- 5 ngày
-       (2, 2, '2024-02-01', '2024-02-03'), -- 2 ngày
-       (2, 5, '2024-04-15', '2024-04-18'), -- 3 ngày
-       (3, 4, '2023-12-20', '2023-12-25'), -- 5 ngày
-       (3, 6, '2024-05-01', '2024-05-06'), -- 5 ngày
-       (4, 1, '2024-06-10', '2024-06-11');
--- 1 ngày
-
--- Phần I:
--- Liệt kê tên khách và số điện thoại của tất cả khách hàng
-select guest_name, phone
-from guests;
--- Liệt kê các loại phòng khác nhau trong khách sạn
-select distinct room_type
-from rooms;
--- Hiển thị loại phòng và giá thuê theo ngày, sắp xếp theo giá tăng dần
-select room_type, price_per_day
-from rooms
-order by price_per_day asc;
--- Hiển thị các phòng có giá thuê lớn hơn 1.000.000
+-- câu 1: tạo view view_studentbasic
+create or replace view view_studentbasic as
+select s.studentid,
+       s.fullname,
+       d.deptname
+from student s
+         join department d on s.deptid = d.deptid;
 select *
-from rooms
-where price_per_day > 1000000;
--- Liệt kê các lần đặt phòng diễn ra trong năm 2024
+from view_studentbasic;
+-- câu 2: tạo regular index cho cột fullname
+create index idx_student_fullname
+    on student (fullname);
+-- câu 3:  Viết Stored Procedure GetStudentsIT
+delimiter //
+create procedure getstudentsit()
+begin
+    select s.studentid,
+           s.fullname,
+           s.gender,
+           s.birthdate,
+           d.deptname
+    from student s
+             join department d on s.deptid = d.deptid
+    where d.deptname = 'information technology';
+end //
+delimiter ;
+call getstudentsit();
+-- câu 4
+-- a:
+create or replace view view_studentcountbydept as
+select d.deptname,
+       count(s.studentid) as totalstudents
+from department d
+         left join student s on d.deptid = s.deptid
+group by d.deptname;
+-- b:
 select *
-from bookings
-where year(check_in) = 2024;
--- Cho biết số lượng phòng của từng loại phòng
-select room_type, count(*) as so_luong_phong
-from rooms
-group by room_type;
--- PHẦN II:
--- Hãy liệt kê danh sách các lần đặt phòng, Với mỗi lần đặt phòng, hãy hiển thị: Tên khách hàng,Loại phòng đã đặt,Ngày nhận phòng (check_in)
-select g.guest_name, r.room_type, b.check_in
-from bookings b
-         join guests g on b.guest_id = g.guest_id
-         join rooms r on b.room_id = r.room_id;
--- Cho biết mỗi khách đã đặt phòng bao nhiêu lần
-select g.guest_name, count(b.booking_id) as so_lan_dat_phong
-from guests g
-         left join bookings b on g.guest_id = b.guest_id
-group by g.guest_id, g.guest_name;
--- Tính doanh thu của mỗi phòng, với công thức: “Doanh thu = số ngày ở × giá thuê theo ngày”
--- Hiển thị tổng doanh thu của từng loại phòng
--- Tìm những khách đã đặt phòng từ 2 lần trở lên
-select g.guest_name,
-       count(b.booking_id) as so_lan_dat
-from guests g
-         join bookings b on g.guest_id = b.guest_id
-group by g.guest_id, g.guest_name
-having count(b.booking_id) >= 2;
--- Tìm loại phòng có số lượt đặt phòng nhiều nhất
--- PHẦN III:
--- Hiển thị những phòng có giá thuê cao hơn giá trung bình của tất cả các phòng
-select *
-from rooms
-where price_per_day > (select avg(price_per_day)
-                       from rooms);
--- Hiển thị những khách chưa từng đặt phòng
-select *
-from guests
-where guest_id not in (
-    select distinct guest_id
-    from bookings
-);
+from view_studentcountbydept
+order by totalstudents desc
+limit 1;
+-- câu 5:
+-- a:
+delimiter //
+create procedure gettopscorestudent(
+    in p_courseid char(6)
+)
+begin
+    select s.studentid,
+           s.fullname,
+           c.coursename,
+           e.score
+    from enrollment e
+             join student s on e.studentid = s.studentid
+             join course c on e.courseid = c.courseid
+    where e.courseid = p_courseid
+      and e.score = (select max(score)
+                     from enrollment
+                     where courseid = p_courseid);
+end //
+delimiter ;
+-- b:
+call gettopscorestudent('c00001');
+-- câu 6:
+-- a
+create or replace view view_it_enrollment_db as
+select e.studentid,
+       e.courseid,
+       e.score
+from enrollment e
+         join student s on e.studentid = s.studentid
+where s.deptid = 'it'
+  and e.courseid = 'c00001'
+with check option;
+-- b
+delimiter //
+create procedure updatescore_it_db(
+    in p_studentid char(6),
+    inout p_newscore float
+)
+begin
+    if p_newscore > 10 then
+        set p_newscore = 10;
+    end if;
+    update view_it_enrollment_db
+    set score = p_newscore
+    where studentid = p_studentid;
+end //
+delimiter ;
